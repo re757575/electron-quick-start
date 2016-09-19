5ecc4dd17b2408b74f9bfb0422e907c82e6b5368
Alex Dai <re757575@gmail.com>
2016-09-19 22:24:10 +0800

    修改webpack設定,依照環境變數變更

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,32 @@ const webpack = require('webpack');
 const webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 const path = require('path');
 
+const plugins = [
+  new webpack.ExternalsPlugin('commonjs', ['fs']),
+  new webpack.IgnorePlugin(/vertx/)
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
+function getEntrySources(sources) {
+  if (process.env.NODE_ENV !== 'production') {
+    sources.push('webpack-dev-server/client?http://localhost:3000');
+    sources.push('webpack/hot/only-dev-server');
+  }
+  return sources;
+}
+
 const config = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './app/index'
-  ],
-  output: {
+  entry: {
+    bundle: getEntrySources([
+      './app/index.js'
+    ])
+  }, output: {
     path: path.join(__dirname, 'app/build'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     publicPath: 'http://localhost:3000/static/'
   },
   module: {
@@ -21,11 +37,7 @@ const config = {
       include: path.join(__dirname, 'app')
     }]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ExternalsPlugin('commonjs', ['fs']),
-    new webpack.IgnorePlugin(/vertx/)
-  ],
+  plugins: plugins
 };
 
 config.target = webpackTargetElectronRenderer(config);
